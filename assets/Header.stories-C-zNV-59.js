519d37a69d54616d4e27711c6ab92c40dc743c73
pauld <pauld@xelure.com>
2025-08-24 19:41:53 +0800

    Updates

---
+++ assets/Header.stories-C-zNV-59.js
@@ -0,0 +1,7 @@
+import{H as a}from"./Header-ChXmZOdc.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-CamNAop3.js";import"./preload-helper-D9Z9MdNV.js";import"./Button-CluhrtB7.js";const{fn:o}=__STORYBOOK_MODULE_TEST__,p={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};const d=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,d as __namedExportsOrder,p as default};
